0a79b650cd189ffe321644d8507f7db76d11d00f
Tanvi Jaiswal <tanvijaiswal034@gmail.com>
2026-08-17 11:52:41 +0530

    Renamed folder names in files and exceptions

---
+++ animal.py
@@ -0,0 +1,4 @@
+def describe_pet(animal_type, pet_name):
+    """Display information about the pet"""
+    print(f"I have a {animal_type.title()}.")
+    print(f"My {animal_type.title()}'s name is {pet_name.title()}.")
